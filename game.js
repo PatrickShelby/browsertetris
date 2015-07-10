7a19a78373e8c18f254561f58f4b5c94f6c56e11
Patrick S. Betti <patreek3@gmail.com>
2015-07-09 20:43:46 -0700

    add image

--- game.js
+++ game.js
@@ -1,11 +1,25 @@
-var canvas = document.createElement("canvas");
-var context = canvas.getContext("2d");
-canvas.width = 300;
-canvas.height = 400;
-document.body.appendChild(canvas);
 
+window.onload = function() {
+  canvasTetris();
+  startGame();
+};
 
-var pyramid = {
-  x:0,
-  y:0
+function canvasTetris() {
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext("2d");
+
+  canvas.height = 600;
+  canvas.width = 300;
+
 }
+
+function pieceDog() {
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext("2d");
+  image = document.getElementById('source');
+    var scaleFactor=0.2;
+  ctx.drawImage(image, 0, 0, image.width*scaleFactor, image.height*scaleFactor);
+
+}
+
+
