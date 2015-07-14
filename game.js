97cd6cfd947ee6f7a5ad4a2e3ebfbd86c8b93d18
Patrick S. Betti <patreek3@gmail.com>
2015-07-13 21:36:16 -0700

    added basic piece stuff

--- game.js
+++ game.js
@@ -1,25 +1,58 @@
 
-window.onload = function() {
-  canvasTetris();
-  startGame();
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 300;
+canvas.height = 600;
+
+
+
+  var piece = function(name) {
+ var array = [];
+ var coordinates = [];
+
+ if (name === "single") {
+    coordinates.push(0,0)
+  }
+
+  if (name === "block") {
+    coordinates.push(0,0,0,1,1,0,1,1)
+  }
+
+  for (i=0;i<coordinates.length;i+=2) {
+    x = coordinates[i]
+    y = coordinates[i + 1]
+    a = new square(x,y)
+    array.push(a)
+  }
+
+  var drop = function() {
+    for (i=1;i<array.length;i++){
+      this.array[i].y += 30;
+    };
+    return this;
+  };
+}
+
+
+var square = function(x, y) {
+  var unit = 30;
+  x = x*unit
+  y = y*unit
+  this.y = y
+  this.x = x
+  ctx.rect(x,y,30,30);
+  ctx.stroke();
+
 };
 
-function canvasTetris() {
-  canvas = document.getElementById("myCanvas");
-  ctx = canvas.getContext("2d");
-
-  canvas.height = 600;
-  canvas.width = 300;
+var gravity = function() {
 
 }
 
-function pieceDog() {
-  canvas = document.getElementById("myCanvas");
-  ctx = canvas.getContext("2d");
-  image = document.getElementById('source');
-    var scaleFactor=0.2;
-  ctx.drawImage(image, 0, 0, image.width*scaleFactor, image.height*scaleFactor);
-
-}
+a = new piece("block")
+a.coordinates;
+a.drop();
 
 
+
+var start = null;
